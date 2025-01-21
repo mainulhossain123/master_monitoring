80d065087e0da7b99e9af0cd1b1b64200929ba43
Mainul Hossain <71011546+mainulhossain123@users.noreply.github.com>
2025-01-21 11:53:36 +0600

    Update master_monitoring_upt.sh

--- master_monitoring_upt.sh
+++ master_monitoring_upt.sh
@@ -48,9 +48,9 @@ shift $((OPTIND - 1))
 # Check if cleanup is requested
 if [ "$CLEANUP" = true ]; then
     echo "Stopping all diagnostic scripts..."
-    ./threadcount/netcore_threadcount_monitoring.sh -c 2>/dev/null
-    ./responsetime/resp_monitoring.sh -c 2>/dev/null
-    ./outboundconnection/snat_monitoring.sh -c 2>/dev/null
+    ./threadcount/netcore_threadcount_monitoring_upt.sh -c 2>/dev/null
+    ./responsetime/resp_monitoring_upt.sh -c 2>/dev/null
+    ./outboundconnection/snat_connection_monitoring.sh -c 2>/dev/null
     kill -SIGTERM $(ps -ef | grep "$master_script_name" | grep -v grep | tr -s " " | cut -d" " -f2 | xargs)
     exit 0
 fi
